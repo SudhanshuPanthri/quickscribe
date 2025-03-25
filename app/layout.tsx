2a2df9166e63badc7a922c1b507473808065ba77
sudhanshupanthri <panthrisudhanshu666@gmail.com>
2025-03-25 19:33:27 +0530

    Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Quickscribe",
+  description: "An AI App that helps you summarize your PDF's",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center font-[family-name:var(--font-geist-sans)]">
+      <Button variant="outline">Hello</Button>
+      <h1>Home</h1>
+    </div>
+  );
+}
