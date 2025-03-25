c75fae952039d0f57123389857d4cca9f5c408fc
sudhanshupanthri <panthrisudhanshu666@gmail.com>
2025-03-25 21:50:13 +0530

    Creating Landing page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/home/hero-section";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
-
+    <div className="relative w-full">
+      <HeroSection />
     </div>
   );
 }
